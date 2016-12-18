21519a992fff0baf38c605956ce9f9c363c1eb6b
Brian Mullen <bmullen@molbie.co>
2016-12-18 11:05:51 -0700

    Add additional casting options for CGFloat

--- Sources/CGFloat+Outlaw.swift
+++ Sources/CGFloat+Outlaw.swift
@@ -10,4 +10,23 @@ import CoreGraphics
 import Outlaw
 
 
-extension CGFloat: Value {}
+extension CGFloat: Value {
+    public static func value(from object: Any) throws -> CGFloat {
+        let value: CGFloat
+        
+        switch object {
+        case let rawValue as Double:
+            value = ValueType(rawValue)
+        case let rawValue as CGFloat:
+            value = ValueType(rawValue)
+        case let rawValue as Float:
+            value = ValueType(rawValue)
+        case let rawValue as NSNumber:
+            value = ValueType(rawValue.doubleValue)
+        default:
+            throw OutlawError.typeMismatch(expected: ValueType.self, actual: type(of: object))
+        }
+        
+        return value
+    }
+}
